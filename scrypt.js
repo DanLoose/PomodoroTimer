3073d5f5ed41b0f26d21f487901d6fc3d337d05d
Danilo Loose <danloose14@gmail.com>
2021-08-16 10:09:07 -0300

    fist commit

---
+++ scrypt.js
@@ -0,0 +1,29 @@
+var button = document.getElementById("button");
+var distance = 1000 * 60 * 25 - 1000;
+
+button.addEventListener("click", timer);
+
+function timer(){
+
+    var x = setInterval(function(){
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+        document.getElementById("timer").innerText = minutes + ":"+  seconds;
+        distance === 0 ? stopTimer(x)  : distance -= 1000;
+
+    }, 1000);
+    
+}
+
+function stopTimer(x){
+    clearInterval(x);
+    alert("Time is over! Take a break from work.");
+}
+
+function mode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var element2 = document.getElementsByClassName("mode");
+    element2[0].classList.toggle("mode-class");
+ }
