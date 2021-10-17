d8dc93444f4472a9ef81bc2877cc2c3114dba574
Danilo Loose <danloose14@gmail.com>
2021-10-17 16:35:03 -0300

    buttons and alert now working correctly

--- JS/interface.js
+++ JS/interface.js
@@ -1,8 +1,19 @@
-const start = document.querySelector("button[name=START]");
-const pause = document.querySelector("button[name=PAUSE]");
+const button = document.querySelector("button[id=button]");
 
 const minutes = document.querySelector("input[id=minutes]");
 const seconds = document.querySelector("input[id=seconds]");
+
+const title = document.querySelector("title");
+
+const isPaused = {
+    flag: false,
+    pauseListener() {
+        return this.flag;
+    },
+    setPause(f) {
+        this.flag = f;
+    }
+}
 
 function handleUserInput(e) {
 
@@ -15,6 +26,11 @@ function handleUserInput(e) {
     } else if (e.value <= 0) {
         e.value = "00";
     }
+
+    if (e.value > 0 && button.getAttribute("name") == "RESET") {
+        handleButton("START");
+    }
+
 }
 
 function startTimer(secs) {
@@ -24,22 +40,43 @@ function startTimer(secs) {
     let updated_min;
     let updated_sec;
 
-    let myInterval = setInterval(() => {
+    var myInterval = setInterval(() => {
 
-        total_secs--;
-
-        updated_min = Math.floor(total_secs / 60);
-        updated_sec = total_secs - 60 * updated_min;
-
-        minutes.value = updated_min;
-        seconds.value = updated_sec;
-
-        handleUserInput(minutes);
-        handleUserInput(seconds);
-
-        if (updated_min == 0 && updated_sec == 0) {
+        if (isPaused.pauseListener()) {
             clearInterval(myInterval);
+        } else {
+            total_secs--;
+
+
+            updated_min = Math.floor(total_secs / 60);
+            updated_sec = total_secs - 60 * updated_min;
+
+            minutes.value = updated_min;
+            seconds.value = updated_sec;
+
+            //IMPLEMENTAÇÕES PENDENTES
+            title.innerHTML = updated_min + ":" + updated_sec;
+
+            handleUserInput(minutes);
+            handleUserInput(seconds);
+
+            if (updated_min == 0 && updated_sec == 0) {
+                handleButton("RESET");
+                clearInterval(myInterval);
+                setTimeout(() => {
+                    alert("time is over!! time for a break.")
+                }, 1000);
+            }
         }
+
+
 
     }, 1000);
 }
+
+function handleButton(option) {
+    // console.log(button);
+    button.innerHTML = option;
+    button.setAttribute("name", option)
+}
+
--- JS/scrypt.js
+++ JS/scrypt.js
@@ -10,8 +10,28 @@ seconds.addEventListener("change", () => {
 })
 
 // ================ SQUARE FOOTER ================
-start.addEventListener("click", () => {
+button.addEventListener("click", () => {
 
+    switch (button.getAttribute("name")) {
+        case "START":
+            start();
+            break;
+        case "PAUSE":
+            pause();
+            break;
+        case "RESET":
+            reset();
+            break;
+        default:
+            console.log("Botão não indentificado");
+    }
+
+})
+
+// ================ CONTROL FUNCTIONS ================
+function start() {
+
+    isPaused.setPause(false);
     let min = Number(minutes.value);
     let sec = Number(seconds.value);
 
@@ -19,4 +39,20 @@ start.addEventListener("click", () => {
 
     startTimer(total_secs);
 
-})
+    handleButton("PAUSE");
+}
+
+function pause() {
+    isPaused.setPause(true);
+    handleButton("START");
+}
+
+function reset() {
+
+    minutes.value = 25;
+    seconds.value = 00;
+    handleUserInput(minutes)
+    handleUserInput(seconds)
+    handleButton("START");
+
+}
